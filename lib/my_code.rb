bb693081a39a8873c43eda753bbe103bd9ae06aa
Harriet Bicknell <harrietindubai@gmail.com>
2019-12-18 10:49:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,18 +1,19 @@
 # Your Code Here
-def map(source_array)
-   new = []
+def map(array)
+  new = []
   counter = 0 
-  while counter < source_array.length do 
-    new.push (source_array[counter])
+  while counter < array.length do 
+    new.push (array[counter])
     counter += 1
   end
   yield new
   new 
 end
 
-map(source_array) {|n| n *-1}
-map(source_array) {|n| n * 2}
-map(source_array) {|n| n * n}
+map(array) {|n| n *-1}
+map(array){|n| n}
+map(array) {|n| n * 2}
+map(array) {|n| n * n}
 
 
  
