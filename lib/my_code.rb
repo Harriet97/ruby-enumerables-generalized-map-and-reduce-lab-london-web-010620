997e863d6db46840c8bcef3ec1c4a248094652b3
Harriet Bicknell <harrietindubai@gmail.com>
2019-12-18 10:48:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,20 @@
 # Your Code Here
+def map(source_array)
+   new = []
+  counter = 0 
+  while counter < source_array.length do 
+    new.push (source_array[counter])
+    counter += 1
+  end
+  yield new
+  new 
+end
+
+map(source_array) {|n| n *-1}
+map(source_array) {|n| n * 2}
+map(source_array) {|n| n * n}
+
+
+ 
+
+
