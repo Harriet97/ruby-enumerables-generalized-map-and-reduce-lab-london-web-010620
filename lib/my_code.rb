0b1080b82afcdfb06990a9b66cde4f7c224a590b
Harriet Bicknell <harrietindubai@gmail.com>
2019-12-18 10:55:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,7 +13,7 @@ def reduce(array, starting_point = 0)
   new = starting_point
   counter = 0 
   while counter < array.length do 
-    new.push (yield(array[counter]))
+    new<< (yield(array[counter]))
     counter += 1
   end
   new 
